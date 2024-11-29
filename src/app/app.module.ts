1c9e7c086c057c5d42ba7cba9b929bca08ad4fcf
ilagunas <ilagunas@pegasus.cl>
2024-11-29 11:12:55 -0300

    Intentando que qr funcione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { IonicStorageModule } from '@ionic/storage-angular';
+  import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { RouteReuseStrategy } from '@angular/router';
+  import { FormsModule } from '@angular/forms';
+  import { IonicStorageModule } from '@ionic/storage-angular';
 
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+  import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {  HttpClientModule } from '@angular/common/http';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
+  import {  HttpClientModule } from '@angular/common/http';
+  import { QRCodeModule } from 'angularx-qrcode';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [HttpClientModule, BrowserModule,IonicStorageModule.forRoot(),FormsModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+  @NgModule({
+    declarations: [AppComponent],
+    imports: [HttpClientModule, QRCodeModule,BrowserModule,IonicStorageModule.forRoot(),FormsModule, IonicModule.forRoot(), AppRoutingModule ],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent],
+  })
+  export class AppModule {}
